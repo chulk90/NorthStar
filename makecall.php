afdda341ef564e39137d9031cf8d3eaee4ae5f44
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 01:44:45 -0500

    change

--- makecall.php
+++ makecall.php
@@ -1,19 +1,19 @@
 <?php
-require "Services/Twilio.php";
+require "twilio-php/Services/Twilio.php";
 
 /* Set our AccountSid and AuthToken */
-$AccountSid = "AXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$AuthToken = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
+$AccountSid = "AC554454875ef2761dfd97bf9f4d438baa";
+$AuthToken = "c2af4f4d0f28090ead35c5b0b4fc8a16";
 
 /* Your Twilio Number or an Outgoing Caller ID you have previously validated
 	with Twilio */
-$from= 'NNNNNNNNNN';
+$from= '3106272485';
 
 /* Number you wish to call */
-$to= 'MMMMMMMMMM';
+$to= '4434747584';
 
 /* Directory location for callback.php file (for use in REST URL)*/
-$url = 'http://www.example.com/clicktocall/';
+$url = 'http://chulk90.azurewebsites.net/';
 
 /* Instantiate a new Twilio Rest Client */
 $client = new Services_Twilio($AccountSid, $AuthToken);
