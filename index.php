3292276c18f3e7c335df8f7b421e414960b22b11
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 10:22:01 -0500

    curiosity

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>NorthStar: Your Guardian from Above</title>
 </head>
 <body>
-<h1>Click-to-call</h1>
+<h1>E-911 testing (voice and SMS)</h1>
 <?php 
 
 if(isset($_REQUEST['msg'])) {
@@ -15,9 +15,8 @@ if(isset($_REQUEST['msg'])) {
 
 <!-- @start snippet -->
 
-<h3>Please enter your phone number, and you will be connected to Chul's phone</h3>
+<h3>Click on 'Go!' when you're ready.</h3>
 <form action="makecall.php" method="post">
-    <span>Your Number: <input type="text" name="called" /></span>
     <input type="submit" value="Connect me!" />
 </form>
 
