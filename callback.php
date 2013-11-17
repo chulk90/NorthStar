2eeaee71408f50a20a61bb711817462a3099ba39
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 11:35:25 -0500

    test - 3

--- callback.php
+++ callback.php
@@ -3,7 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>This is an E nine one one service request. There was an accident at Duke University (Latitude: 36.003458, Longitude: -78.939671) 37 seconds ago. Please dispatch an ambulance immediately.</Say>
-
+    <Say>This is an E nine one one service request. There was an accident at Duke University (Latitude: 36.003458, Longitude: -78.939671) 37 seconds ago. Please dispatch an ambulance immediately. Again, Latitude: 36.003458, Longitude: -78.939671. Reference number of this call is 12.</Say>
+    <Sms>This is an E-911 request. There was an accident in Duke University (36.003458,-78.939671) 38 seconds ago. Please dispatch an ambulance immediately. Reference #: 12"</Sms>
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
--- makecall.php
+++ makecall.php
@@ -16,7 +16,7 @@ $to= '4434747584';
 $url = 'http://chulk90.azurewebsites.net/';
 
 /* SMS: Message */
-$message = "This is an E-911 request. There was an accident in Duke University (36.003458,-78.939671) 38 seconds ago. Please dispatch an ambulance immediately. Reference #: 24";
+$message = "This is an E-911 request. There was an accident in Duke University (36.003458,-78.939671) 38 seconds ago. Please dispatch an ambulance immediately. Reference #: 12";
 
 /* Instantiate a new Twilio Rest Client */
 $client = new Services_Twilio($AccountSid, $AuthToken);
