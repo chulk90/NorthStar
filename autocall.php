c8f970c49f52221cf518a39170f5bbd5e97d48e3
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 13:21:07 -0500

    cosmetic change

---
+++ autocall.php
@@ -0,0 +1,19 @@
+<?php ?>
+<htmL>
+<head>
+	<title>NorthStar: Your Guardian from Above</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+</head>
+<body>
+
+<?php 
+
+//if(isset($_REQUEST['msg'])) {
+//	echo '<i>' . $_REQUEST['msg'] . '</i>';
+//}
+include("makecall.php")
+?>
+
+</body>
+</html>
+
