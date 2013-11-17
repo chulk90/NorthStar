a9326f407c25e93ce0b4125b1b3d7f015fa474b5
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 09:58:34 -0500

    Callback: Change in Message

--- callback.php
+++ callback.php
@@ -3,6 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>A customer at the number <?php echo $_REQUEST['number']?> is calling</Say>
+    <Say>This is an E-911 service request. There was an accident in Duke University (Latitude: 36.003458, Longitude: -78.939671) two minutes ago. Please dispatch an ambulance immediately.</Say>
+
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
