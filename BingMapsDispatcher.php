822a1767a18516e287ab2d88a8cf41cfb5f7ab18
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 11:02:58 -0500

    Fixing Bing Maps - Part 2

--- BingMapsDispatcher.php
+++ BingMapsDispatcher.php
@@ -10,46 +10,54 @@
       <script type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>
 
       <script type="text/javascript">
-      function GetMap()
-      {   
+        function GetMap()
+        {   
 
-        var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
-                           {credentials: "AhXzZBKRsnTVM7M-LOFjoATuQPXll5Ufx25UIZNS2RgHNPmyBg1bh5KMzKxm3EmC",
-                            center: new Microsoft.Maps.Location(36.003458, -78.939671),
-                            mapTypeId: Microsoft.Maps.MapTypeId.road,
-                            zoom: 17});
+          var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
+                             {credentials: "AhXzZBKRsnTVM7M-LOFjoATuQPXll5Ufx25UIZNS2RgHNPmyBg1bh5KMzKxm3EmC",
+                              center: new Microsoft.Maps.Location(36.003458, -78.939671),
+                              mapTypeId: Microsoft.Maps.MapTypeId.road,
+                              zoom: 17});
 
-/*
-        // Retrieve the location of the map center 
-        var center = map.getCenter();
-            
-        // Add a pin to the center of the map
-        var pin = new Microsoft.Maps.Pushpin(center, {text: '1'}); 
+  /*
+          // Retrieve the location of the map center 
+          var center = map.getCenter();
+              
+          // Add a pin to the center of the map
+          var pin = new Microsoft.Maps.Pushpin(center, {text: '1'}); 
 
-        // Create the info box for the pushpin
-        pinInfobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(36.003458, -78.939671), {title: 'Location of the Caller', visible: true});
+          // Create the info box for the pushpin
+          pinInfobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(36.003458, -78.939671), {title: 'Location of the Caller', visible: true});
 
-        // Add a handler for the pushpin click event.
-        Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+          // Add a handler for the pushpin click event.
+          Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
 
-        // Hide the info box when the map is moved.
-        Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
+          // Hide the info box when the map is moved.
+          Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
 
-        // Add the pushpin and info box to the map
-        map.entities.push(pin);
-        map.entities.push(pinInfobox);
-*/
-        // Retrieve the location of the map center 
-            var center = map.getCenter();
-            
-            // Add a pin to the center of the map
-            var pin = new Microsoft.Maps.Pushpin(center, {draggable: true}); 
+          // Add the pushpin and info box to the map
+          map.entities.push(pin);
+          map.entities.push(pinInfobox);
+  */
+          // Retrieve the location of the map center 
+              var center = map.getCenter();
+              
+              // Add a pin to the center of the map
+              var pin = new Microsoft.Maps.Pushpin(center, {draggable: true}); 
 
-            // Add a handler to the pushpin drag
-            Microsoft.Maps.Events.addHandler(pin, 'mouseup', DisplayLoc);
+              // Add a handler to the pushpin drag
+              Microsoft.Maps.Events.addHandler(pin, 'mouseup', DisplayLoc);
 
-            map.entities.push(pin);
-      }
+              map.entities.push(pin);
+        }
+
+        function DisplayLoc(e){
+          if (e.targetType == 'pushpin'){
+
+             var pinLoc = e.target.getLocation();
+             alert("The location of the pushpin is now " + pinLoc.latitude + ", " + pinLoc.longitude);
+           }
+        }
       </script>
    </head>
    <body onload="GetMap();">
