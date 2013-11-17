2560615c2d5e3108de447516990224a5bd301fc7
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 10:39:16 -0500

    Adding Bing Maps - Part 1

---
+++ BingMapsDispatcher - Intro.php
@@ -0,0 +1,28 @@
+<?php ?>
+<htmL>
+<head>
+	<title>Bing Maps for Dispatcher</title>
+</head>
+<body>
+<h1>Where exactly is the patient?</h1>
+<?php 
+
+/*
+if(isset($_REQUEST['msg'])) {
+	echo '<i>' . $_REQUEST['msg'] . '</i>';
+}
+*/
+
+?>
+
+<!-- @start snippet -->
+
+<h3>Type in the call's reference number:</h3>
+<form action="BingMapsDispatcher.html" method="post">
+	<span>Reference Number: <input type="text" name="called" /></span>
+    <input type="submit" value="Connect me!" />
+</form>
+
+<!-- @end snippet -->
+</body>
+</html>
