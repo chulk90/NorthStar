b7e67bbe3691b73c898a156fe537594fae936dda
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 10:55:08 -0500

    Adding Bing Maps - Part 5
    Infobox

--- BingMapsDispatcher.php
+++ BingMapsDispatcher.php
@@ -13,15 +13,34 @@
       function GetMap()
       {   
 
-         var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
+        var map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), 
                            {credentials: "AhXzZBKRsnTVM7M-LOFjoATuQPXll5Ufx25UIZNS2RgHNPmyBg1bh5KMzKxm3EmC",
                             center: new Microsoft.Maps.Location(36.003458, -78.939671),
                             mapTypeId: Microsoft.Maps.MapTypeId.road,
                             zoom: 17});
+
+        // Retrieve the location of the map center 
+        var center = map.getCenter();
+            
+        // Add a pin to the center of the map
+        var pin = new Microsoft.Maps.Pushpin(center, {text: '1'}); 
+
+        // Create the info box for the pushpin
+        pinInfobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(36.003458, -78.939671), {title: 'Location of the Caller', visible: true});
+
+        // Add a handler for the pushpin click event.
+        Microsoft.Maps.Events.addHandler(pin, 'click', displayInfobox);
+
+        // Hide the info box when the map is moved.
+        Microsoft.Maps.Events.addHandler(map, 'viewchange', hideInfobox);
+
+        // Add the pushpin and info box to the map
+        map.entities.push(pin);
+        map.entities.push(pinInfobox);
       }
       </script>
    </head>
    <body onload="GetMap();">
-      <div id='mapDiv' style="position:relative; width:400px; height:400px;"></div>       
+      <div id='mapDiv' style="position:relative; width:800px; height:450px;"></div>       
    </body>
 </html>
