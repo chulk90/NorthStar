06e0f10453bfe7facc455f26e4f041a481928f25
Chul Kwon <chulk90+github@gmail.com>
2013-11-17 10:17:00 -0500

    minor formatting issue

--- callback.php
+++ callback.php
@@ -3,7 +3,7 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>This is an E-9-1-1 service request. There was an accident in Duke University (Latitude: 36.003458, Longitude: -78.939671) one minute and 36 seconds ago. Please dispatch an ambulance immediately.</Say>
+    <Say>This is an E nine one one service request. There was an accident at Duke University (Latitude: 36.003458, Longitude: -78.939671) one minute and 36 seconds ago. Please dispatch an ambulance immediately.</Say>
 
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
