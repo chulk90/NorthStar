9c26a5db1ccd643e3a8daa71a02e1d2873bdaa4e
Chul Kwon <chulk90+github@gmail.com>
2014-03-20 21:16:02 -0400

    test1 callback.php

--- callback.php
+++ callback.php
@@ -3,7 +3,11 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>This is an E nine one one service request. There was an accident at Duke University (Latitude: 36.003458, Longitude: -78.939671) 37 seconds ago. Please dispatch an ambulance immediately. Again, Latitude: 36.003458, Longitude: -78.939671. Reference number of this call is 12.</Say>
+    <Say>Yo Sun Mee Zle! This is Char, your boo!</Say>
+
+    <!--
+    This is an E nine one one service request. There was an accident at Duke University (Latitude: 36.003458, Longitude: -78.939671) 37 seconds ago. Please dispatch an ambulance immediately. Again, Latitude: 36.003458, Longitude: -78.939671. Reference number of this call is 12.
+    -->
     
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
