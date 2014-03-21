4b5130d4b7f3e217e95ca4766d47eeb8065b32da
Chul Kwon <chulk90+github@gmail.com>
2014-03-21 14:37:09 -0700

    Now expand to time

--- callback.php
+++ callback.php
@@ -3,6 +3,6 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Say>This is a test. One, two, three, four, five. <?php echo $_REQUEST['location']?></Say>
+    <Say>There was a motorcycle collision in <?php echo $_REQUEST['location']?> at <?php echo $_REQUEST['time']?> No fire has been detected, and the victim appears to be in a stable condition.</Say>
     <Dial><?php echo $_REQUEST['number']?></Dial>
 </Response>
